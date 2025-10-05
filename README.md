1b11828a469e11b216486c8c65fe2f382c389008
AntoineBastide47 <148970403+AntoineBastide47@users.noreply.github.com>
2025-10-05 13:20:35 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Meeple-Meet
+
---
+++ app/src/main/java/com/github/meeplemeet/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.github.meeplemeet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+/**
+ * `MainActivity` is the entry point of the application. It sets up the content view with the
+ * `onCreate` methods. You can run the app by running the `app` configuration in Android Studio. NB:
+ * Make sure you have an Android emulator running or a physical device connected.
+ */
+class MainActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+        setContent { Surface(modifier = Modifier.fillMaxSize()) { Text("Hello World") } }
+    }
+}
