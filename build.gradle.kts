ffb0e222f8deb1d944c92954abf0353688839999
AntoineBastide47 <148970403+AntoineBastide47@users.noreply.github.com>
2025-10-06 21:04:24 +0200

    chore: remove sonar file paths

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,6 @@ sonar {
         property("sonar.projectKey", "Meeple-Meet_Meeple-Meet")
         property("sonar.organization", "meeple-meet")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.sources", "app/src/main/java")
-        property("sonar.tests", "app/src/test/java,app/src/androidTest/java")
-        property("sonar.java.binaries", "app/build/tmp/kotlin-classes/debug")
         property("sonar.coverage.jacoco.xmlReportPaths", "app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
         property("sonar.androidLint.reportPaths", "app/build/reports/lint-results-debug.xml")
     }
