bd635689a56af2fd3c3a62eb8ef3afc81dcc58d8
AntoineBastide47 <148970403+AntoineBastide47@users.noreply.github.com>
2025-10-06 20:22:52 +0200

    fix: add solar cube back to the CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,18 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.ktfmt) apply false
     alias(libs.plugins.gms) apply false
+    id("org.sonarqube") version "5.1.0.4882"
+}
 
+sonar {
+    properties {
+        property("sonar.projectKey", "Meeple-Meet_Meeple-Meet")
+        property("sonar.organization", "meeple-meet")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.sources", "app/src/main/java")
+        property("sonar.tests", "app/src/test/java,app/src/androidTest/java")
+        property("sonar.java.binaries", "app/build/tmp/kotlin-classes/debug")
+        property("sonar.coverage.jacoco.xmlReportPaths", "app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
+        property("sonar.androidLint.reportPaths", "app/build/reports/lint-results-debug.xml")
+    }
 }
