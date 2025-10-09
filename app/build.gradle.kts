d1b1b39579e6b4cdffcdad1efbb72afbd9acee32
AntoineBastide47 <148970403+AntoineBastide47@users.noreply.github.com>
2025-10-09 15:55:51 +0200

    chore: try again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -212,7 +212,7 @@ tasks.withType<Test> {
 }
 
 tasks.register("jacocoTestReport", JacocoReport::class) {
-  dependsOn("testDebugUnitTest")  // Changed from mustRunAfter to dependsOn
+  dependsOn("testDebugUnitTest")
   mustRunAfter("connectedDebugAndroidTest")
 
   reports {
@@ -244,6 +244,7 @@ tasks.register("jacocoTestReport", JacocoReport::class) {
     include("outputs/code_coverage/debugAndroidTest/connected/*/coverage.ec")
   })
 }
+
 configurations.forEach { configuration ->
   // Exclude protobuf-lite from all configurations
   // This fixes a fatal exception for tests interacting with Cloud Firestore
