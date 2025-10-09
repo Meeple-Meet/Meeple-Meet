60eb9aaf2e20e30f9c68641cad4eabb280e2f606
AntoineBastide47 <148970403+AntoineBastide47@users.noreply.github.com>
2025-10-09 18:19:11 +0200

    chore: fix CI AGAIN !

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,11 @@ sonar {
         property("sonar.projectKey", "Meeple-Meet_Meeple-Meet")
         property("sonar.organization", "meeple-meet")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.coverage.jacoco.xmlReportPaths", "app/build/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
-        property("sonar.androidLint.reportPaths", "app/build/reports/lint-results-debug.xml")
+
+        property("sonar.exclusions", "**/*.png,**/*.jpg,**/*.jpeg,**/*.gif,**/*.webp,**/*.ttf,**/*.otf,**/*.woff,**/*.woff2,**/*.ico")
+        property("sonar.sources", "app/src/main/java,app/src/main/kotlin")
+        property("sonar.tests", "app/src/test,app/src/androidTest")
+
+        property("sonar.sourceEncoding", "UTF-8")
     }
 }
