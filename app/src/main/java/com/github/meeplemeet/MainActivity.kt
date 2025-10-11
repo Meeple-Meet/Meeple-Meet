ab9a29fa3853cdfadfea63348dcb5ff914dabe59
EnesKiper <eneskiper67@gmail.com>
2025-10-11 18:43:19 +0200

    Reset files to match main branch versions

---
+++ app/src/main/java/com/github/meeplemeet/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.github.meeplemeet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.github.meeplemeet.ui.theme.AppTheme
+
+/**
+ * `MainActivity` is the entry point of the application. It sets up the content view with the
+ * `onCreate` methods. You can run the app by running the `app` configuration in Android Studio. NB:
+ * Make sure you have an Android emulator running or a physical device connected.
+ */
+class MainActivity : ComponentActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent { AppTheme { Surface(modifier = Modifier.fillMaxSize()) { Text("Hello World") } } }
+  }
+}
