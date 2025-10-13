85cfd37ab2824d7db61a763e5a2aa9c840dd598c
AntoineBastide47 <148970403+AntoineBastide47@users.noreply.github.com>
2025-10-13 16:12:41 +0200

    chore: fix CI coverage not downloading lint report

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,12 @@ sonar {
         property("sonar.androidLint.reportPaths", "${project.layout.buildDirectory.get()}/reports/lint-results-debug.xml")
         // Paths to JaCoCo XML coverage report files.
         property("sonar.coverage.jacoco.xmlReportPaths", "${project.layout.buildDirectory.get()}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
+        
+        property("sonar.exclusions", "**/*.png,**/*.jpg,**/*.jpeg,**/*.gif,**/*.webp,**/*.ttf,**/*.otf,**/*.woff,**/*.woff2,**/*.eot,**/*.svg")
+        property("sonar.test.exclusions", "**/androidTest/**,**/debug/**,**/test/**")
+
+        property("sonar.pullrequest.provider", "Github")
+        property("sonar.sourceEncoding", "UTF-8")
     }
 }
 
