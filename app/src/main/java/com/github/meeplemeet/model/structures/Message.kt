a310bb2b208d8413a8efee479e855aae4ac7f975
EnesKiper <eneskiper67@gmail.com>
2025-10-29 11:18:34 +0100

    Adding polling class and updating message class

--- app/src/main/java/com/github/meeplemeet/model/structures/Message.kt
+++ app/src/main/java/com/github/meeplemeet/model/structures/Message.kt
@@ -6,15 +6,17 @@ import kotlinx.serialization.Serializable
 /**
  * Represents a single message within a discussion.
  *
- * Stored inside the `messages` list of a [Discussion] document.
+ * Stored inside the `messages` list of a [Discussion] document. Can optionally contain a poll.
  *
  * @property senderId UID of the account that sent this message.
  * @property content Text body of the message.
  * @property createdAt Timestamp of when the message was created on the server.
+ * @property poll Optional poll data attached to this message.
  */
 @Serializable
 data class Message(
     val senderId: String = "",
     val content: String = "",
-    val createdAt: Timestamp = Timestamp.now()
+    val createdAt: Timestamp = Timestamp.now(),
+    val poll: Poll? = null
 )
