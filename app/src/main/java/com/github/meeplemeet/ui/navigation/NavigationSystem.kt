fadd9c604b32b7d1fe24e650bb2b7f776cc2ebb7
danym <danymoukaddam@gmail.com>
2025-10-31 00:10:12 +0100

    feat: integrate feedsOverviewScreen with nav system

--- app/src/main/java/com/github/meeplemeet/ui/navigation/NavigationSystem.kt
+++ app/src/main/java/com/github/meeplemeet/ui/navigation/NavigationSystem.kt
@@ -107,6 +107,7 @@ enum class MeepleMeetScreen(
   AddSession("Add Session"),
   Session("Session"),
   SessionDetails("Session Details"),
+  FeedsOverview("Feeds"),
   CreatePost("Create post")
 }
 
