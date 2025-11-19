b441557e9e2f27d8bee6f2d1ad019a1d15553b4b
Thomas Picart <thomas.picart90@gmail.com>
2025-11-19 19:01:00 +0100

    fix(bgg): lazy init gamesApi in MainActivity

--- app/src/main/java/com/github/meeplemeet/MainActivity.kt
+++ app/src/main/java/com/github/meeplemeet/MainActivity.kt
@@ -37,6 +37,7 @@ import com.github.meeplemeet.model.map.PinType
 import com.github.meeplemeet.model.map.StorableGeoPinRepository
 import com.github.meeplemeet.model.posts.PostRepository
 import com.github.meeplemeet.model.sessions.SessionRepository
+import com.github.meeplemeet.model.shared.game.BggGameRepository
 import com.github.meeplemeet.model.shared.game.FirestoreGameRepository
 import com.github.meeplemeet.model.shared.location.LocationRepository
 import com.github.meeplemeet.model.shared.location.NominatimLocationRepository
@@ -121,6 +122,7 @@ object RepositoryProvider {
 
   /** Lazily initialized repository for board game data operations. */
   val games: FirestoreGameRepository by lazy { FirestoreGameRepository() }
+  val gamesApi: BggGameRepository by lazy { BggGameRepository() }
 
   /** Lazily initialized repository for location operations. */
   val locations: LocationRepository by lazy { NominatimLocationRepository() }
