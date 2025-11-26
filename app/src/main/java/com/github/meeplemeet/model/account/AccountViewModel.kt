430dd5d302c39f3f4e854f1bda904ae363af4af0
MarcoLourenco55 <marco.lourencoleitao@epfl.ch>
2025-11-26 16:21:06 +0100

    refactor(ui): use ```require``` instead of if-condition
    Ref: issue https://github.com/Meeple-Meet/Meeple-Meet/issues/182

--- app/src/main/java/com/github/meeplemeet/model/account/AccountViewModel.kt
+++ app/src/main/java/com/github/meeplemeet/model/account/AccountViewModel.kt
@@ -26,7 +26,7 @@ interface AccountViewModel {
    * @throws IllegalArgumentException if the ID is blank
    */
   fun getAccount(id: String) {
-    if (id.isBlank()) throw IllegalArgumentException(BLANK_ACCOUNT_ID_ERROR)
+    require(id.isNotBlank()) { BLANK_ACCOUNT_ID_ERROR }
     scope.launch { RepositoryProvider.accounts.getAccount(id, false) }
   }
 
@@ -62,7 +62,7 @@ interface AccountViewModel {
    * @throws IllegalArgumentException if the ID is blank
    */
   fun getOtherAccount(id: String, onResult: (Account) -> Unit) {
-    if (id.isBlank()) throw IllegalArgumentException(BLANK_ACCOUNT_ID_ERROR)
+    require(id.isNotBlank()) { BLANK_ACCOUNT_ID_ERROR }
     scope.launch { onResult(RepositoryProvider.accounts.getAccount(id, false)) }
   }
 
@@ -74,7 +74,7 @@ interface AccountViewModel {
    * @param onResult Callback that receives the retrieved account
    */
   fun getOtherAccount(id: String, context: Context, onResult: (Account) -> Unit) {
-    if (id.isBlank()) throw IllegalArgumentException(BLANK_ACCOUNT_ID_ERROR)
+    require(id.isNotBlank()) { BLANK_ACCOUNT_ID_ERROR }
     scope.launch {
       val account = RepositoryProvider.accounts.getAccount(id, false)
       onResult(account)
