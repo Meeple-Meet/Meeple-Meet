723c921d2107d4e40f83d7d450bc3ea5d84451cf
EnesKiper <eneskiper67@gmail.com>
2025-12-04 11:58:01 +0100

    Format code

--- app/src/main/java/com/github/meeplemeet/ui/sessions/SessionDetailsScreen.kt
+++ app/src/main/java/com/github/meeplemeet/ui/sessions/SessionDetailsScreen.kt
@@ -57,7 +57,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.testTag
